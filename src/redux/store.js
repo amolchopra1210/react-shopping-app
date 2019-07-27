9ee53669911f969659a73a5dcac3155295f1d734
Amol <amolviper@gmail.com>
2019-07-27 21:30:53 +0530

    added redux persist to persist state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
 import {createStore, applyMiddleware} from 'redux'
-import reducer from './root-reducer';
+import {persistStore} from 'redux-persist';
 import logger from 'redux-logger'
+import reducer from './root-reducer';
  
 const middleware = [logger]
 
 export const store = createStore(reducer,applyMiddleware(...middleware));
+
+export const persistor = persistStore(store);
+
+export default {store, persistor};
+ 
 
